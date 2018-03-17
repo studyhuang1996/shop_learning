b477f49909b63d8e8b0e6bf85f25a4dabdd7d192
studyhuang1996 <studyhuang1996@gmail.com>
2018-03-17 18:44:41 +0800

    用户模块接口实现

--- src/main/java/com/studyhuang/dao/UserMapper.java
+++ src/main/java/com/studyhuang/dao/UserMapper.java
@@ -1,6 +1,8 @@
 package com.studyhuang.dao;
 
 import com.studyhuang.pojo.User;
+import org.apache.ibatis.annotations.Param;
+
 
 public interface UserMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +16,25 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+
+    /**
+     *判断用户名是否存在
+     * @param username
+     * @return
+     */
+    int checkName(String username);
+
+    int checkEmail(String email);
+
+    User login(@Param("username") String username, @Param("password") String password);
+
+    String selectQuestionByUserName(String username);
+
+    int checkAnswer(@Param("username") String username,@Param("question") String question,@Param("answer") String answer);
+
+    int updatePassword(@Param("username") String username,@Param("passwordNew") String pwdmd5);
+
+    int updateOldPassword(@Param("password") String oldpwd,@Param("user_id") Integer id);
+
+    int selectEmial(@Param("userId") Integer id,@Param("email") String email);
 }
